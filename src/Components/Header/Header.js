460eee1d1c497b45d3777e9e184c99c0ca7006ab
Ramiro D'Accorso <ramiro.daccorso@gmail.com>
2020-07-06 21:23:19 -0300

    Creando header y esqueleto basico del proyecto

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Nav, Link,Linka } from './styles'
+import { FaTwitch,FaHome,FaTwitter } from 'react-icons/fa'
+import { MdCropLandscape } from 'react-icons/md'
+
+const TEXTSIZE = '26px';
+export const Header = () => {
+  return(
+    <Nav>
+      <Linka target="_blank" href="https://www.twitch.tv/dami_r92"><FaTwitch size={TEXTSIZE}/></Linka>
+      <Linka  target="_blank" href='https://twitter.com/dami_rk92'><FaTwitter size={TEXTSIZE}/></Linka>
+      <Link to='/home'><FaHome size={TEXTSIZE}/></Link>
+      <Link to='/participantes'><MdCropLandscape size={TEXTSIZE} /></Link>
+      <Link to='/resultados'></Link>
+    </Nav>
+  )
+}
---
+++ src/styles/GlobalStyled.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+body {
+  background: #fefefe;
+  height: 100vh;
+  margin: 0 auto;
+  max-width: 500px;
+  overscroll-behavior: none;
+  width: 100%;
+}
+
+`
